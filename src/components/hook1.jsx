15943de6c553128b56541f2d1348841f7954f0a2
shuang.wang02 <shuang.wang02@ele.me>
2018-11-26 00:01:35 +0800

    test useEffect

--- src/components/hook1.jsx
+++ src/components/hook1.jsx
@@ -6,7 +6,13 @@ function Example() {
   const [age, setAge] = useState(18);
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  });
+    return () => {
+      // 在组件卸载时执行
+      //  If your effect returns a function, React will run it when it is time to clean up:
+      console.log('测试')
+    }
+  },[]);// Only re-run the effect if count changes 
+  // 数组里面可以存储状态 指定改变的状态执行useEffect
   return (
     <div>
       <p>You clicked {count} times</p>
@@ -17,6 +23,13 @@ function Example() {
         }}
       >
         click me
+      </button>
+      <button
+        onClick = {()=>{
+          setAge(age+1)
+        }}
+      >
+        change age
       </button>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
 import Example from "./components/hook1.jsx";
 import "./styles.css";
 
 function App() {
+  const [status, setStatus] = useState(true)
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Example />
+      <button onClick = { () => {
+        setStatus(!status);
+      }}> change components status</button>
+      {
+        status && <Example />
+      }
     </div>
   );
 }
